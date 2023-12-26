926a02df6d292f0a42190062cd5515c24716af21
Tom Smeets <tom@tsmeets.nl>
2023-12-26 22:36:48 +0100

    ui: start desiging the api

--- app/quest_for_nothing.c
+++ app/quest_for_nothing.c
@@ -14,34 +14,7 @@
 #include "mat.h"
 #include "sound.h"
 #include "gfx.h"
-
-static void input_key_dbg(input *in) {
-    mem m = {};
-    for (u32 i = 0; i < array_count(in->key); ++i) {
-        input_key *key = in->key + i;
-        if (!key->code) break;
-        if (!key->click) continue;
-        os_print(fmt(&m, "key=%s\n", enum_to_str_input_key_code[key->code]));
-    }
-    mem_clear(&m);
-}
-
-
-static void audio_callback(f32 dt, u32 count, v2 *buffer) {
-    static sound snd_static;
-    sound *snd = &snd_static;
-
-    for (u32 i = 0; i < count; ++i) {
-        snd_begin(snd, dt, 120);
-
-        f32 v = 0;
-        v += snd_sin(snd, 110)*snd_sin(snd, 1);
-        v += snd_sin(snd, 220)*snd_sin(snd, 2);
-        v += snd_sin(snd, 440)*snd_sin(snd, 3);
-        v = snd_compress(v, snd->beat % 4 + 1);
-        buffer[i] = (v2){v, v};
-    }
-}
+#include "ui.h"
 
 struct App {
     mem tmp;
@@ -51,67 +24,69 @@ struct App {
 
     sdl_win *window;
 
+    u64 dt;
     u64 time;
     u64 start_time;
 
     gl_t *gl;
+    UI *ui;
 };
-
-static void play(char *music) {
-    char *p = music;
-
-    while(*p) {
-        while(*p == ' ') p++;
-        Note n = parse_note(&p);
-        printf("%f\n", n.freq);
-    }
-}
 
 // You can choose how to run this app
 // - dynamically: use ./hot main.so
 // - directly:    use ./main
 void *main_init(int argc, char **argv) {
-    os_printf("main_init called :)\n");
     mem m = {};
     App *app = mem_struct(&m, App);
     global_set(&app->global);
-
     app->perm = m;
     app->window = sdl_new(&m, "Hello World");
     app->start_time = os_time();
     app->time = app->start_time;
     app->gl   = gl_init(&m, app->window->gl);
+    app->dt   = 1000 * 1000 / 200;
+    app->ui   = mem_struct(&m, UI);
     return app;
 }
 
 void main_update(void *handle) {
     App *app = handle;
-    global_set(&app->global);
     mem *m = &app->tmp;
 
     sdl_win *win = app->window;
     gl_api  *gl  = win->gl;
-    u32 rate = 200;
+    UI *ui = app->ui;
 
-    // os_printf("time:    %f\n", (app->time - app->start_time) / 1e6);
-    // os_printf("Counter: %u\n", app->counter++);
+    global_set(&app->global);
     sdl_begin(win);
-    // win->audio_callback = audio_callback;
 
-    gl_draw(app->gl, win->input.window_size);
-
-    input_key_dbg(&win->input);
+    // Handle quit
     if (win->input.quit || input_is_down(&win->input, KEY_Q)) {
         sdl_quit(win);
         os_exit(0);
     }
 
+    // os_printf("%f %f\n", win->input.mouse_pos.x, win->input.mouse_pos.y);
+
+    // Draw ui
+    ui_begin(ui, &win->input, m);
+
+    if(ui_button(ui, "Click Me!")) {
+        os_print("Hello, World!\n");
+    }
+
+    if(ui_button(ui, "Other Button")) {
+        os_print("Other Button!\n");
+    }
+
+    ui_newline(ui);
+    ui_button(ui, "A");
+    ui_button(ui, "B");
+    ui_end(ui);
+
     sdl_end(win);
 
-    // The best way to do timing
-    // 1. compute target future time: t1 = t0 + dt
-    // 2. sleep until we reach that time
-    // if we miss our mark a little we can correct it again the next frame
-    app->time += 1000 * 1000 / rate;
+    // Wait for the next frame
+    app->time += app->dt;
     os_sleep_until(app->time);
 }
--- src/ui.h
+++ src/ui.h
@@ -1,38 +1,76 @@
 // Copyright (c) 2023 - Tom Smeets <tom@tsmeets.nl>
 // ui.h - Immediate mode UI
 #pragma once
+#include "os.h"
 #include "inc.h"
 #include "vec.h"
 #include "gfx.h"
+#include "input.h"
 
 // Simple grid like layout
 struct UI {
     u32 index;
-    gfx_pass *gfx;
-    input *input;
+    u32 active;
 
-    v2 component_size;
-    v2 cursor;
+    // size of one component (height)
+    f32 size;
+    f32 pad;
+
+    // where we are currently drawing
+    v2 pos;
+
+    input *input;
 };
 
 
 static void ui_begin(UI *ui, input *input, mem *m) {
-    ui->index = 0;
-    ui->gfx = gfx_pass_new(m);
+    ui->index = 1;
+    ui->size = 80;
+    ui->pad   = 4;
+    ui->pos   = (v2) { 0, 0 };
     ui->input = input;
+
+    if(ui->active && !input_is_down(ui->input, KEY_MOUSE_LEFT))
+        ui->active = 0;
 }
 
 static gfx_pass *ui_end(UI *ui) {
+    return 0;
 }
 
-
 static bool ui_button(UI *ui, const char *text) {
-    v2 l = ui->cursor;
-    v2 h = l + ui->component_size;
-    ui->cursor.x = h.x;
+    u32 id = ui->index++;
+
+    v2 outer_min = ui->pos;
+    v2 outer_max = ui->pos + (v2) { 2 * ui->size, ui->size };
+
+    v2 inner_min = outer_min + (v2) { ui->pad, ui->pad };
+    v2 inner_max = outer_max - (v2) { ui->pad, ui->pad };
+
+    bool hover = ui->input->mouse_pos.x >= inner_min.x && ui->input->mouse_pos.x < inner_max.x
+              && ui->input->mouse_pos.y >= inner_min.y && ui->input->mouse_pos.y < inner_max.y;
+
+    bool down  = hover && input_is_down(ui->input, KEY_MOUSE_LEFT);
+    bool click = hover && input_is_click(ui->input, KEY_MOUSE_LEFT);
+
+    if(click && !ui->active)
+        ui->active = id;
+
+    if(ui->active != id) {
+        hover = 0;
+        down = 0;
+        click = 0;
+    }
+
+    if(click) {
+        os_printf("Clicked Button: %s\n", text);
+    }
+
+    ui->pos.x = outer_max.x;
+    return click;
 }
 
 static void ui_newline(UI *ui) {
-    ui->cursor.x = 0;
-    ui->cursor.y += ui->component_size.y;
+    ui->pos.y += ui->size;
+    ui->pos.x = 0;
 }
