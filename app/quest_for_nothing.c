030ebce1c5c9edf5ca169ffa90adfd39feaa2d44
Tom Smeets <tom@tsmeets.nl>
2024-01-23 22:25:14 +0100

    quest_for_nothing: Remove strange line

--- app/quest_for_nothing.c
+++ app/quest_for_nothing.c
@@ -59,7 +59,6 @@ void main_update(void *handle) {
     mem *tmp = &app->tmp;
 
     Sdl *win = app->window;
-    UI *ui = app->ui;
 
     global_set(&app->global);
     sdl_begin(win);
@@ -73,42 +72,36 @@ void main_update(void *handle) {
 
     // os_printf("%f %f\n", win->input.mouse_pos.x, win->input.mouse_pos.y);
 
-    // Draw ui
-    ui_begin(ui, &win->input, tmp);
-
-    if(ui_button(ui, "Click Me!")) {
-        os_print("Hello, World!\n");
-    }
-
-    if(ui_button(ui, "Other Button")) {
-        os_print("Other Button!\n");
-    }
-
-    ui_newline(ui);
-    ui_button(ui, "A");
-    ui_button(ui, "B");
-    ui_end(ui);
-    gl_draw(app->gl, ui->gfx);
-
-    // Draw a mouse cursor
     {
+        // Draw ui
+        UI *ui = app->ui;
+        ui_begin(ui, &win->input, tmp);
+        if(ui_button(ui, "Click Me!")) {
+            os_print("Hello, World!\n");
+        }
+
+        if(ui_button(ui, "Other Button")) {
+            os_print("Other Button!\n");
+        }
+        ui_newline(ui);
+        ui_button(ui, "A");
+        ui_button(ui, "B");
+        ui_end(ui);
+        gl_draw(app->gl, ui->gfx);
+    }
+
+    {
+        // Draw a mouse cursor
         Gfx *gfx = gfx_begin(tmp);
-        gfx_stroke_width(gfx, 8);
-        // m4_scale(&gfx->mtx, (v3) { 20, 20, 1});
-        // m4_rot_z(&gfx->mtx, (app->time % 4000000) / 4000000.0f * -R4);
-        // m4_trans(&gfx->mtx, (v3){ win->input.mouse_pos.x, win->input.mouse_pos.y });
         m4_screen_to_clip(&gfx->mtx, win->input.window_size);
-
-        gfx_color(gfx, BLACK);
-        gfx_line(gfx, win->input.window_size/2, win->input.mouse_pos);
-
-        gfx_color(gfx, WHITE);
-        gfx_stroke_width(gfx, 4);
-        gfx_line(gfx, win->input.window_size/2, win->input.mouse_pos);
-
         gfx_color(gfx, RED);
-        gfx_circle(gfx, win->input.mouse_pos, 20);
+        gfx_circle(gfx, win->input.mouse_pos, 4);
         gl_draw(app->gl, gfx);
+    }
+
+    {
+        // Draw something 3d
+
     }
 
     sdl_end(win);
