e249d7bfd7a48e5f00e4059c9a13c8105b4a2cfa
Tom Smeets <tom@tsmeets.nl>
2024-12-03 20:54:51 +0100

    Build: add default

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 #
 # Copyright (c) 2024 - Tom Smeets <tom@tsmeets.nl>
 # build.sh - Build the current project
-clang -o out/hot src/hot.c && ./out/hot "$@"
+clang -o out/hot src/hot.c && ./out/hot "${@:-build}"
