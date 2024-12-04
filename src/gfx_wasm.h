d96d3fe4542066aae65087e0434956f83a6dceef
Tom Smeets <tom@tsmeets.nl>
2024-12-04 22:20:30 +0100

    cleanup

--- src/gfx_wasm.h
+++
@@ -1,3 +0,0 @@
-// Copyright (c) 2024 - Tom Smeets <tom@tsmeets.nl>
-// gfx.h - Gfx implementation for the web
-#pragma once
--- src/ogl.h
+++
@@ -1,3 +0,0 @@
-// Copyright (c) 2024 - Tom Smeets <tom@tsmeets.nl>
-// ogl.h - OpenGL 3.3 helper methods
-#pragma once
