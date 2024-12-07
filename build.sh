8ecdbcd222481e2e2d0b168272d693a5f3957971
Tom Smeets <tom@tsmeets.nl>
2024-12-07 14:00:22 +0100

    simplify build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 #
 # Copyright (c) 2024 - Tom Smeets <tom@tsmeets.nl>
 # build.sh - Build the current project
-mkdir -p out && clang -o out/hot src/hot.c && ./out/hot "${@:-build}"
+mkdir -p out && clang -o out/hot src/hot.c && ./out/hot "${@}"
