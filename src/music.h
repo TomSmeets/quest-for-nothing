a5b4feb905d0d5e9e50b252115f89cce0eba886a
Tom Smeets <tom@tsmeets.nl>
2025-02-09 18:45:14 +0100

    music: more crappy music

--- src/music.h
+++ src/music.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
-// music.h - Music generation
+// music.h - Everything to do with music synthesis
 #pragma once
 #include "audio.h"
 #include "engine.h"
@@ -8,7 +8,6 @@
 
 // 4/4 timing
 // | 0123 | 4567 | 89ab | cdef |
-
 typedef struct {
     f32 sleep_time;
     i32 note;
@@ -29,20 +28,20 @@ typedef struct {
 // starts at C0..
 // Each octave is 7 notes
 // C4 = 7*4
-#define MUSIC_C4 (7*4)
+#define MUSIC_C4 (7 * 4)
 static f32 music_note_to_freq(u32 note) {
     assert(note <= 7 * 8, "only 8 supported ocatves");
 
     f32 octave_list[9] = {
         1.0f / 16.0f, // 0
-        1.0f / 8.0f, // 1
-        1.0f / 4.0f, // 2
-        1.0f / 2.0f, // 3
-        1.0f, // 4
-        2.0f, // 5
-        4.0f, // 6
-        8.0f, // 7
-        16.0f, // 8
+        1.0f / 8.0f,  // 1
+        1.0f / 4.0f,  // 2
+        1.0f / 2.0f,  // 3
+        1.0f,         // 4
+        2.0f,         // 5
+        4.0f,         // 6
+        8.0f,         // 7
+        16.0f,        // 8
     };
 
     // A - F
@@ -65,6 +64,11 @@ static f32 music_note_to_freq(u32 note) {
 }
 
 static void music_init(Music *music) {
+    // u32 notes[3][] = {
+    //     { 1, 1, 2, 2 }, // Note
+    //     { 1, 1, 1, 1 }, // Length
+    // }
+    // u32 notes[] = {}
 }
 
 static void music_play(Music *music, Engine *eng) {
@@ -72,7 +76,7 @@ static void music_play(Music *music, Engine *eng) {
     if (music->sleep_time > 0) return;
 
     u32 beat_count = 4;
-    f32 beat_per_second = 70.0f / 60.0f / 4;
+    f32 beat_per_second = 70.0f / 60.0f;
     f32 dt = 1.0f / beat_per_second;
     music->sleep_time += dt * beat_count;
 
@@ -82,24 +86,48 @@ static void music_play(Music *music, Engine *eng) {
     for (u32 i = 0; i < beat_count; ++i) {
         fmt_s(OS_FMT, "Submitting beats\n");
 
-        Sound snd = {};
-        snd.src_a.freq = 1;
-        snd.src_a.volume = .25;
-        snd.src_a.attack_time = dt;
-        snd.src_a.release_time = dt;
+        {
+            Sound snd = {};
+            snd.freq = music_note_to_freq(music->note + 4 * 7);
+            snd.src_a.freq = 1;
+            snd.src_a.volume = .25;
+            snd.src_a.attack_time = 0.1;
+            snd.src_a.release_time = dt * 2;
 
-        snd.src_b.freq = 1.0f;
-        snd.src_b.volume = 1;
-        snd.src_b.attack_time = dt;
-        snd.src_b.release_time = dt;
+            snd.src_b.freq = 1.0f;
+            snd.src_b.volume = 1.0;
+            snd.src_b.attack_time = 0.2;
+            snd.src_b.release_time = dt * 2;
 
-        snd.src_c.freq = 2;
-        snd.src_c.volume = 1;
-        snd.src_c.attack_time = dt;
-        snd.src_c.release_time = dt;;
-        snd.freq = music_note_to_freq(music->note + 7 * 4);
-        snd.time = time;
-        audio_play(eng->audio, snd);
+            snd.src_c.freq = 1;
+            snd.src_c.volume = 0;
+            snd.src_c.attack_time = 0.1;
+            snd.src_c.release_time = dt * 2;
+            snd.time = time;
+            audio_play(eng->audio, snd);
+        }
+
+        {
+            Sound snd = {};
+            snd.freq = music_note_to_freq(music->note + 3 * 7);
+            snd.src_a.freq = 1;
+            snd.src_a.volume = .125;
+            snd.src_a.attack_time = 0.2;
+            snd.src_a.release_time = dt;
+
+            snd.src_b.freq = 1.0f;
+            snd.src_b.volume = 8.0;
+            snd.src_b.attack_time = 0.2;
+            snd.src_b.release_time = dt;
+
+            snd.src_c.freq = 1;
+            snd.src_c.volume = 0;
+            snd.src_c.attack_time = 0.5;
+            snd.src_c.release_time = dt;
+            snd.time = time;
+            audio_play(eng->audio, snd);
+        }
+
         time -= 1.0f * dt;
 
         if (rand_u32(&eng->rng) % 2 == 0) {
@@ -108,7 +136,7 @@ static void music_play(Music *music, Engine *eng) {
             music->note -= 1;
         }
 
-        if(music->note > 7) music->note = 6;
-        if(music->note < -7) music->note = -6;
+        if (music->note > 7) music->note = 6;
+        if (music->note < -7) music->note = -6;
     }
 }
