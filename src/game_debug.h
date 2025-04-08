2f5a6eeaf656803ab800f3603efd5d9821922207
Tom Smeets <tom@tsmeets.nl>
2025-04-08 23:26:42 +0200

    fix compilation

--- src/game_debug.h
+++ src/game_debug.h
@@ -12,7 +12,7 @@ static void sparse_debug_draw(Engine *eng, Sparse_Set *set, Entity *ent) {
                     gfx_debug_box(eng->gfx_dbg, cell->box, 0);
                     for (Sparse_Node *node = cell->nodes; node; node = node->next) {
                         gfx_debug_box(eng->gfx_dbg, node->box, 1);
-                        v3 p = v3i_to_v3(cell->pos) * SPARSE_BOX_SIZE;
+                        v3 p = v3i_to_v3(cell->pos) * SPARSE_CELL_SIZE;
                         gfx_debug_line(eng->gfx_dbg, p, box_center(node->box), 2);
                     }
                 }
