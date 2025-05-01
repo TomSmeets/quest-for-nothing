4e0d3909408a138736651ea3dacc02e87ff199d2
Tom Smeets <tom@tsmeets.nl>
2025-05-01 23:57:05 +0200

    types: remove result

--- lib/types.h
+++ lib/types.h
@@ -45,9 +45,3 @@ static_assert(sizeof(bool) == 1);
 
 #define offset_of(type, field) __builtin_offsetof(type, field)
 #define array_count(a) (sizeof(a) / sizeof(a[0]))
-
-typedef enum {
-    OK,
-    ERROR_OUT_OF_MEMORY,
-    ERROR_FILE_NOT_FOUND,
-} Result;
