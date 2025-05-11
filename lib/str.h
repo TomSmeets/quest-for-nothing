b32c0d7a255d252b59be5d744912acd6c4f764bf
Tom Smeets <tom@tsmeets.nl>
2025-05-11 16:46:33 +0200

    str: add String datatype

--- lib/str.h
+++ lib/str.h
@@ -2,6 +2,23 @@
 // str.h - simple string handling functions
 #pragma once
 #include "types.h"
+#include "std.h"
+
+// utf8 encoded string datatype
+// String is NOT zero terminated
+typedef struct {
+    // NOTE: Maximum size is 4 GB (mem chunk is only 1 MB anyway)
+    // NOTE: Use 'Text' for a string that can be any size
+    u32 len;
+    u8 *data;
+} String;
+
+// Construct a String from a constant expression
+// NOTE: Only use with constant string literals
+#define S(x) ((String){sizeof(x) - 1, (u8 *)x})
+
+// A Zero/Null/Empty String
+#define S0 ((String){0})
 
 // Number of chars in a zero terminated string
 static u32 str_len(char *s) {
@@ -10,28 +27,53 @@ static u32 str_len(char *s) {
     return n;
 }
 
-static bool str_eq(char *s1, char *s2) {
-    if (s1 == s2) return true;
-    for (;;) {
-        if (*s1 != *s2) return false;
-        if (*s1 == 0) return true;
-        s1++;
-        s2++;
-    }
+// Convert a dynamic zero terminated string to a String
+static String str_from(char *data) {
+    return (String){str_len(data), (u8 *)data};
 }
 
-static bool str_ends_with(char *str, char *end) {
-    u32 l1 = str_len(str);
-    u32 l2 = str_len(end);
-    if (l2 > l1) return false;
-    return str_eq(str + l1 - l2, end);
+static bool str_eq(String s1, String s2) {
+    if (s1.len != s2.len) return false;
+    return std_memcmp(s1.data, s2.data, s1.len);
 }
 
-static bool str_starts_with(char *str, char *start) {
-    for (;;) {
-        if (*start == 0) return true;
-        if (*start != *str) return false;
-        start++;
-        str++;
-    }
+// Create a substring
+static String str_slice(String str, u32 offset, u32 size) {
+    if (offset > str.len) return S0;
+    if (offset + size > str.len) size = str.len - offset;
+    str.data += offset;
+    str.len = size;
+    return str;
+}
+
+// Take n chars from the start
+static String str_take_start(String str, u32 n) {
+    if(n > str.len) return str;
+    return str_slice(str, 0, n);
+}
+
+// Take n chars from the end
+static String str_take_end(String str, u32 n) {
+    if(n > str.len) return str;
+    return str_slice(str, str.len - n, n);
+}
+
+// Take n chars from the start
+static String str_drop_start(String str, u32 n) {
+    if(n >= str.len) return S0;
+    return str_slice(str, n, str.len - n);
+}
+
+// Take n chars from the end
+static String str_drop_end(String str, u32 n) {
+    if (n >= str.len) return S0;
+    return str_slice(str, 0, str.len - n);
+}
+
+static bool str_ends_with(String str, String end) {
+    return str_eq(str_take_end(str, end.len), end);
+}
+
+static bool str_starts_with(String str, String start) {
+    return str_eq(str_take_start(str, start.len), start);
 }
--- lib/str_mem.h
+++ lib/str_mem.h
@@ -4,20 +4,115 @@
 #include "mem.h"
 #include "str.h"
 
-static char *str_dup(char *str, Memory *mem) {
-    u32 len = str_len(str);
-    char *cpy = mem_push_uninit(mem, len + 1);
-    std_memcpy(cpy, str, len + 1);
-    return cpy;
+// Allocate a new and empty string
+static String str_alloc(Memory *mem, u32 len) {
+    String str = {
+        .len = len,
+        .data = mem_push_uninit(mem, len + 1),
+    };
+    str.data[str.len] = 0;
+    return str;
 }
 
-static char *str_cat3(Memory *mem, char *s0, char *s1, char *s2) {
-    u32 l0 = str_len(s0);
-    u32 l1 = str_len(s1);
-    u32 l2 = str_len(s2);
-    char *ret = mem_push_uninit(mem, l0 + l1 + l2);
-    std_memcpy(ret, s0, l0);
-    std_memcpy(ret + l0, s1, l1);
-    std_memcpy(ret + l0 + l1, s2, l2 + 1); // include zero byte
+// Delte chars from `pos` until `pos + del` and replace it with `new`
+// The result is stored in a new allocated String, the original string is not modified
+static String str_replace(Memory *mem, String str, u32 pos, u32 del, String new) {
+    assert(str.len >= pos + del, "Out of range");
+    String str2 = str_alloc(mem, str.len - del + new.len);
+    std_memcpy(str2.data + 0, str.data, pos);
+    std_memcpy(str2.data + pos, new.data, new.len);
+    std_memcpy(str2.data + pos + new.len, str.data + pos + del, str.len - pos - del);
+    return str2;
+}
+
+// Append two strings
+// The result is stored in a new allocated String, the original string is not modified
+static String str_append(Memory *mem, String s1, String s2) {
+    String ret = str_alloc(mem, s1.len + s2.len);
+    std_memcpy(ret.data, s1.data, s1.len);
+    std_memcpy(ret.data + s1.len, s2.data, s2.len);
     return ret;
+}
+
+// Allocate a new copy of `str`
+static String str_clone(Memory *mem, String str) {
+    String new = str_alloc(mem, str.len);
+    std_memcpy(new.data, str.data, str.len);
+    return new;
+}
+
+static void test_str(void) {
+    Memory *mem = mem_new();
+
+    // `S0` and `S()`
+    assert(str_eq(S0, S0) == 1, "Zero string is equal to itself");
+    assert(str_eq(S(""), S0) == 1, "Any empty string is equal to the zero string");
+    assert(str_eq(S(""), S("")) == 1, "All empty strings are equal");
+    assert(str_eq(S("A"), S0) == 0, "Non-empty strings are not equal to empty strings");
+    assert(str_eq(S0, S("A")) == 0, "Non-empty strings are not equal to empty strings (reverse order)");
+    assert(str_eq(S("A"), S("A")) == 1, "Strings can be equal");
+    assert(str_eq(S("Hello"), S("World")) == 0, "Differfent strings should not be equal");
+    assert(str_eq(S("Hello"), S("Hello")) == 1, "Same strings should be equal");
+
+    // str_eq()
+    String s_hello = S("Hello ");
+    String s_world = S("World!");
+    assert0(str_eq(s_hello, s_hello) == true);
+    assert0(str_eq(s_world, s_world) == true);
+    assert0(str_eq(s_hello, s_world) == false);
+    assert0(str_eq(s_world, s_hello) == false);
+
+    // str_slice()
+    assert0(str_eq(str_slice(S("ABCDEF"), 0, 6), S("ABCDEF")));
+    assert0(str_eq(str_slice(S("ABCDEF"), 0, 3), S("ABC")));
+    assert0(str_eq(str_slice(S("ABCDEF"), 3, 3), S("DEF")));
+    assert0(str_eq(str_slice(S("ABCDEF"), 2, 1), S("C")));
+
+    // str_starts_with()
+    assert0(str_ends_with(S("ABCDEF"), S("F")) == 1);
+    assert0(str_ends_with(S("ABCDEF"), S("DEF")) == 1);
+    assert0(str_ends_with(S("ABCDEF"), S("ABCDEF")) == 1);
+    assert0(str_ends_with(S("ABCDEF"), S("XABCDEF")) == 0);
+    assert0(str_ends_with(S("ABCDEF"), S("ABCDEFX")) == 0);
+    assert0(str_ends_with(S("ABCDEF"), S("X")) == 0);
+    assert0(str_ends_with(S("ABCDEF"), S("")) == 1);
+    assert0(str_ends_with(S(""), S("")) == 1);
+    assert0(str_ends_with(S(""), S("A")) == 0);
+
+    // str_ends_with()
+    assert0(str_starts_with(S("ABCDEF"), S("A")) == 1);
+    assert0(str_starts_with(S("ABCDEF"), S("ABC")) == 1);
+    assert0(str_starts_with(S("ABCDEF"), S("ABCDEF")) == 1);
+    assert0(str_starts_with(S("ABCDEF"), S("ABCDEFX")) == 0);
+    assert0(str_starts_with(S("ABCDEF"), S("XABCDEF")) == 0);
+    assert0(str_starts_with(S("ABCDEF"), S("X")) == 0);
+    assert0(str_starts_with(S("ABCDEF"), S("")) == 1);
+    assert0(str_starts_with(S(""), S("")) == 1);
+    assert0(str_starts_with(S(""), S("A")) == 0);
+
+    // str_take() / str_drop()
+    assert(str_eq(str_take_start(S("Hello World!"), 5), S("Hello")), "Take Start");
+    assert(str_eq(str_take_end(S("Hello World!"), 6), S("World!")), "Take End");
+    assert(str_eq(str_drop_start(S("Hello World!"), 6), S("World!")), "Drop Start");
+    assert(str_eq(str_drop_end(S("Hello World!"), 7), S("World!")), "Drop End");
+
+    // str_append()
+    String s_greeting = str_append(mem, s_hello, s_world);
+    assert(str_eq(s_hello, S("Hello ")), "Original should not be changed");
+    assert(str_eq(s_world, S("World!")), "Original should not be changed");
+    assert(str_eq(s_greeting, S("Hello World!")), "Strings should be concatinated");
+
+    // str_replace()
+    assert(str_eq(str_replace(mem, S("Hello test World!"), 6, 4, S("AWESOME")), S("Hello AWESOME World!")), "Replace test");
+    assert(str_eq(str_replace(mem, S("Hello test World!"), 6, 5, S0), S("Hello World!")), "Delete test");
+    assert(str_eq(str_replace(mem, S("Hello test World!"), 0, 6, S0), S("test World!")), "Delete string-start");
+    assert(str_eq(str_replace(mem, S("Hello test World!"), 0, 17, S0), S0), "Delte everything");
+    assert(str_eq(str_replace(mem, S0, 0, 0, S0), S0), "Edge case");
+
+    // str_clone()
+    String s7 = str_clone(mem, s_hello);
+    assert0(str_eq(s7, s_hello));
+    assert0(s7.data != s_hello.data);
+
+    mem_free(mem);
 }
