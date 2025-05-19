46f0dc0fca0d6136213d860853a0bde344b0edaf
Tom Smeets <tom@tsmeets.nl>
2025-05-19 22:51:20 +0200

    rename fiels

--- src/build/build.c
+++ src/build/build.c
@@ -1,221 +1,238 @@
-// Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
-// build.c - Dynamically compile and reload interactive programs
 #include "clang.h"
-#include "cli.h"
+#include "cli2.h"
 #include "fmt.h"
-#include "fs.h"
 #include "hot.h"
 #include "include_graph.h"
-#include "mem.h"
 #include "os_main.h"
-#include "rand.h"
-#include "tmpfs.h"
-#include "types.h"
 #include "watch.h"
 
-// Check if file exists
-static bool fs_exists(char *path) {
+struct App {
+    // Permanent memory
+    Memory *mem;
+
+    // Per-frame memory
+    Memory *tmp;
+
+    Watch watch;
+
+    // Build
+    bool do_build;
+    Clang_Options build_opts;
+
+    // Hot
+    Hot *hot;
+    String hot_output;
+    Fmt hot_output_fmt;
+
+    // First time?
+    bool first;
+    bool changed;
+};
+
+static bool build_read_opts(Cli *cli, Clang_Options *opts) {
+    opts->input_path = cli_value(cli, "<Input>", "Source Input Path");
+    if (!opts->input_path) return false;
+
+    opts->output_path = cli_value(cli, "<Output>", "Execurtable Output Path");
+    if (!opts->output_path) return false;
+
+    while (cli_read(cli)) {
+        if (0) {
+        } else if (cli_flag(cli, "linux", "Compile for Linux")) {
+            opts->platform = Platform_Linux;
+        } else if (cli_flag(cli, "windows", "Compile for Windows")) {
+            opts->platform = Platform_Windows;
+        } else if (cli_flag(cli, "wasm", "Compile for Webassembly")) {
+            opts->platform = Platform_Wasm;
+        } else if (cli_flag(cli, "release", "Compile in Release Mode")) {
+            opts->release = true;
+        } else if (cli_flag(cli, "dynamic", "Create a dynamic executable (a .dll/.so file)")) {
+            opts->dynamic = true;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool build_format(App *app, Cli *cli) {
+    if (!cli_flag(cli, "format", "Run code formatter")) return false;
+    bool result = os_system(S("clang-format --verbose -i src/*/*.{h,c}"));
+    assert(result, "Format failed!");
+    os_exit(0);
+    return true;
+}
+
+static bool build_include_graph(App *app, Cli *cli) {
+    bool build = cli_flag(cli, "include-graph", "Generate Include graph");
+    if (!build) return false;
+    Include_Graph *graph = include_graph_new(app->tmp);
+    include_graph_read_dir(graph, S("src"));
+    include_graph_tred(graph);
+    // include_graph_rank(graph);
+    include_graph_fmt(graph, G->fmt);
+    os_exit(0);
+    return true;
+}
+
+// Reset formatter and create a unique output path for a .so
+static char *hot_fmt(Fmt *fmt) {
+    fmt->used = 0;
+    fmt_su(fmt, "out/hot_", os_time(), ".so");
+    return fmt_close(fmt);
+}
+
+static bool build_run(App *app, Cli *cli) {
+    // Check command
+    if (!cli_flag(cli, "run", "Run an application with dynamic hot reloading")) return false;
+
+    char *input_path = cli_value(cli, "<INPUT>", "Input file");
+
+    if (!input_path) {
+        cli_show_usage(cli, G->fmt);
+        os_exit(1);
+    }
+
+    // Init hot (if first time)
+    if (!app->hot) {
+        char **hot_argv = cli->argv + cli->ix - 1;
+        u32 hot_argc = cli->argc - cli->ix + 1;
+        app->hot = hot_new(app->mem, hot_argc, hot_argv);
+    }
+
+    if (app->changed) {
+        // Remove previous output file
+        if (app->hot_output.len) {
+            fs_remove(app->hot_output);
+        }
+
+        // Format new output file
+        char *out_path = hot_fmt(&app->hot_output_fmt);
+        app->hot_output = str_from(out_path);
+        fmt_ss(G->fmt, "OUT: ", out_path, "\n");
+
+        Clang_Options opts = {
+            .input_path = input_path,
+            .output_path = out_path,
+            .dynamic = true,
+        };
+
+        if (clang_compile(app->tmp, opts)) {
+            hot_load(app->hot, out_path);
+        }
+    }
+
+    hot_update(app->hot);
+    return true;
+}
+
+static bool build_build(App *app, Cli *cli) {
+    bool build = cli_flag(cli, "build", "Build an executable");
+    bool watch = cli_flag(cli, "watch", "Build an executable and watch changes");
+    if (!build && !watch) return false;
+    if (watch && !app->changed) return true;
+
+    Clang_Options opts = {};
+    if (!build_read_opts(cli, &opts)) {
+        cli_show_usage(cli, G->fmt);
+        os_exit(1);
+    }
+
+    if (!clang_compile(app->mem, opts)) os_exit(1);
+    if (build) os_exit(0);
+    return true;
+}
+
+static bool build_serve(App *app, Cli *cli) {
+    if (!cli_flag(cli, "serve", "Start a simple local python http server for testing wasm builds")) return false;
+    assert(os_system(S("cd out && python -m http.server")), "Failed to start python http server. Is python installed?");
+    os_exit(0);
+    return true;
+}
+
+static bool fs_exists(String path) {
     File *f = os_open(path, Open_Read);
     if (!f) return false;
     os_close(f);
     return true;
 }
 
-static void sdl2_download_dll(void) {
-    // Skip if already downloaded
-    if (fs_exists("out/SDL2.dll")) return;
+static bool build_all(App *app, Cli *cli) {
+    bool build = cli_flag(cli, "release", "Build qfn release");
+    bool upload = cli_flag(cli, "publish", "Upload archive");
+    if (!build && !upload) return false;
+
+    Clang_Options opts = {};
+    opts.input_path = "src/qfn/main.c";
+    opts.release = true;
+
+    opts.platform = Platform_Linux;
+    opts.output_path = "out/release/quest_for_nothing.elf";
+    if (!clang_compile(app->mem, opts)) os_exit(1);
+
+    opts.platform = Platform_Windows;
+    opts.output_path = "out/release/quest_for_nothing.exe";
+    if (!clang_compile(app->mem, opts)) os_exit(1);
+
+    opts.platform = Platform_Wasm;
+    opts.output_path = "out/release/quest_for_nothing.wasm";
+    if (!clang_compile(app->mem, opts)) os_exit(1);
 
     // Download sdl2
-    os_system("curl -L -o out/SDL2.zip https://github.com/libsdl-org/SDL/releases/download/release-2.30.6/SDL2-2.30.6-win32-x64.zip");
-#if OS_IS_WINDOWS
-    os_system("cd out/ && tar -xf SDL2.zip SDL2.dll");
-#else
-    os_system("cd out/ && unzip SDL2.zip SDL2.dll");
-#endif
-}
-
-static bool build_debug(Memory *tmp) {
-    if (!clang_compile(tmp, (Clang_Options){"out/build", "src/build.c", Platform_Linux, false, false})) return 0;
-    if (!clang_compile(tmp, (Clang_Options){"out/main", "src/main.c", Platform_Linux, false, false})) return 0;
-    return 1;
-}
-
-static bool build_release(Memory *tmp, bool release) {
-    if (!os_system("mkdir -p out/release")) return 0;
-    if (!clang_compile(tmp, (Clang_Options){"out/release/quest-for-nothing.elf", "src/main.c", Platform_Linux, release, false})) return 0;
-    if (!clang_compile(tmp, (Clang_Options){"out/release/quest-for-nothing.exe", "src/main.c", Platform_Windows, release, false})) return 0;
-    if (!clang_compile(tmp, (Clang_Options){"out/release/quest-for-nothing.wasm", "src/main.c", Platform_Wasm, release, false})) return 0;
-
-    sdl2_download_dll();
-#if OS_IS_WINDOWS
-    if (!os_system("COPY src\\os_wasm.html out\\release\\index.html")) return 0;
-    if (!os_system("COPY out\\SDL2.dll out\\release\\SDL2.dll")) return 0;
-#else
-    if (!os_system("cp src/os_wasm.html out/release/index.html")) return 0;
-    if (!os_system("cp lib/*.js out/release/")) return 0;
-    if (!os_system("cp out/SDL2.dll out/release/SDL2.dll")) return 0;
-#endif
-    return 1;
-}
-
-// Implementation
-// typedef void os_main_t(OS *os);
-
-static char *build_and_load(Memory *tmp, char *main_path) {
-    // Generate 'asset.h'
-#if OS_IS_WINDOWS
-    sdl2_download_dll();
-#endif
-
-#if OS_IS_WINDOWS
-    char *out_path = tmpfs_path(tmp, "out/tmp", "hot.dll");
-#else
-    char *out_path = tmpfs_path(tmp, "out/tmp", "hot.so");
-#endif
-
-    bool ok = clang_compile(
-        tmp,
-        (Clang_Options){
-            .output_path = out_path,
-            .input_path = main_path,
-            .platform = Platform_Linux,
-            .release = false,
-            .dynamic = true,
-        }
-    );
-
-    if (!ok) {
-        fmt_s(G->fmt, "Compile error!\n");
-        return 0;
+    if (!fs_exists(S("out/SDL2.dll"))) {
+        os_system(S("curl -L -o out/SDL2.zip https://github.com/libsdl-org/SDL/releases/download/release-2.30.6/SDL2-2.30.6-win32-x64.zip"));
+        os_system(S("cd out/ && unzip SDL2.zip SDL2.dll"));
     }
+    os_system(S("cp out/SDL2.dll out/release/SDL2.dll"));
+    os_system(S("cp src/lib/*.js out/release/"));
+    os_system(S("cp src/lib/os_wasm.html out/release/index.html"));
 
-    return out_path;
+    if(upload) {
+        os_system(S("butler push out/release tsmeets/quest-for-nothing:release --userversion $(date +'%F')"));
+        os_system(S("butler push out/release tsmeets/quest-for-nothing:release-web --userversion $(date +'%F')"));
+        os_system(S("rclone copy out/release fastmail:tsmeets.fastmail.com/files/tsmeets.nl/qfn/"));
+    }
+    os_exit(0);
+    return true;
 }
 
-typedef struct App App;
 
-struct App {
-    // Run and hot reload single executable
-    bool action_run;
-    char *main_path;
-
-    // Build everything
-    bool action_build;
-
-    // For generating unique '.so' names
-    Rand rng;
-    Watch watch;
-    bool first_time;
-
-    Hot *hot;
-};
-
-static void exit_with_help(Cli *cli) {
-    char *name = cli->argv[0];
-    cli_show_help(cli);
-    fmt_s(G->fmt, "\n");
-    fmt_s(G->fmt, "Examples:\n");
-    fmt_ss(G->fmt, "  ", name, " run src/main.c\n");
-    fmt_ss(G->fmt, "  ", name, " run src/build.c watch\n");
-    fmt_ss(G->fmt, "  ", name, " build\n");
-    fmt_ss(G->fmt, "  ", name, " release\n");
-    fmt_ss(G->fmt, "  ", name, " asset\n");
-    os_exit(1);
-}
-
-static App *build_init(void) {
-    OS *os = G->os;
-
-    Memory *mem = mem_new();
-    Memory *tmp = mem_new();
-    App *hot = mem_struct(mem, App);
-    Cli *cli = cli_new(tmp, os->argc, os->argv);
-
-    if (cli_action(cli, "run", "<main> [args]...", "Build and run with hot reloading")) {
-        if (os->argc < 3) {
-            fmt_s(G->fmt, "Not enogh arguments\n");
-            exit_with_help(cli);
-        }
-
-        hot->action_run = true;
-        hot->main_path = os->argv[2];
-        hot->hot = hot_new(mem, os->argc - 2, os->argv + 2);
-    } else if (cli_action(cli, "watch", "", "Build all targets and rebuild on every change")) {
-        hot->action_build = true;
-    } else if (cli_action(cli, "build", "", "Build for only one target for quick Debugging")) {
-        build_debug(tmp);
-        os_exit(0);
-    } else if (cli_action(cli, "debug", "", "Build all targets in Debug mode")) {
-        build_release(tmp, false);
-        os_exit(0);
-    } else if (cli_action(cli, "release", "", "Build all targets in Release mode")) {
-        build_release(tmp, true);
-        os_exit(0);
-    } else if (cli_action(cli, "upload", "", "Build release and upload to https://tsmeets.itch.io/quest-for-nothing and https://tsmeets.nl/qfn")) {
-        if (!build_release(tmp, true)) os_exit(1);
-        os_system("butler push out/release tsmeets/quest-for-nothing:release --userversion $(date +'%F')");
-        os_system("butler push out/release tsmeets/quest-for-nothing:release-web --userversion $(date +'%F')");
-        os_system("rclone copy out/release fastmail:tsmeets.fastmail.com/files/tsmeets.nl/qfn/");
-        os_exit(0);
-    } else if (cli_action(cli, "serve", "", "Start a simple local python http server for testing the web version")) {
-        assert(os_system("cd out && python -m http.server"), "Failed to start python http server. Is python installed?");
-        os_exit(0);
-    } else if (cli_action(cli, "format", "", "Format code")) {
-        assert(os_system("clang-format --verbose -i */*.{h,c}"), "Format failed!");
-        os_exit(0);
-    } else if (cli_action(cli, "include-graph", "", "Generate Include graph")) {
-        Include_Graph *graph = include_graph_new(mem);
-        include_graph_read_dir(graph, "src", "red");
-        include_graph_read_dir(graph, "lib", "blue");
-        include_graph_read_dir(graph, "app_build", "green");
-        include_graph_read_dir(graph, "app_qfn", "green");
-        include_graph_tred(graph);
-        // include_graph_rank(graph);
-        include_graph_fmt(graph, G->fmt);
-        os_exit(0);
+static void build_init(App *app, Cli *cli) {
+    if (build_run(app, cli)) {
+    } else if (build_format(app, cli)) {
+    } else if (build_build(app, cli)) {
+    } else if (build_include_graph(app, cli)) {
+    } else if (build_serve(app, cli)) {
+    } else if (build_all(app, cli)) {
     } else {
-        exit_with_help(cli);
+        cli_show_usage(cli, G->fmt);
+        os_exit(1);
     }
 
-    // Init inotify
-    watch_add(&hot->watch, "lib");
-    watch_add(&hot->watch, "src");
-    hot->first_time = 1;
-    hot->rng = rand_fork(G->rand);
-    mem_free(tmp);
-    return hot;
+    // We keep going, so init watch
+    if (!app->watch.count) {
+        watch_add(&app->watch, "src");
+    }
+    app->changed = watch_check(&app->watch);
 }
 
 static void os_main(void) {
-    // Call Constructor
-    if (!G->app) {
-        fs_mkdir("out/tmp");
-        tmpfs_clear("out/tmp");
-        G->app = build_init();
+    App *app = G->app;
+    os_set_update_time(G->os, 100 * 1000);
+    if (!app) {
+        Memory *mem = mem_new();
+        app = G->app = mem_struct(mem, App);
+        app->mem = mem;
+        app->changed = true;
     }
+    app->tmp = mem_new();
 
-    App *hot = G->app;
-    OS *os = G->os;
+    Cli cli = cli_new(G->os->argc, G->os->argv);
+    build_init(app, &cli);
+    // if (watch_check(&app->watch) || app->first) {
+    //     app->first = false;
+    //     if (app->do_build) clang_compile(app->tmp, app->build_opts);
+    // }
 
-    Memory *tmp = mem_new();
-
-    bool changed = watch_check(&hot->watch) || hot->first_time;
-    hot->first_time = 0;
-
-    // Default update rate
-    os->sleep_time = 100 * 1000;
-
-    if (hot->action_run) {
-        if (changed) {
-            char *so_path = build_and_load(tmp, hot->main_path);
-            hot_load(hot->hot, so_path);
-        }
-
-        hot_update(hot->hot);
-    }
-
-    if (hot->action_build && changed) {
-        build_debug(tmp);
-    }
-    mem_free(tmp);
+    mem_free(app->tmp);
 }
