f634b44940dd1a4fd05940990a683196628d5a54
Tom Smeets <tom@tsmeets.nl>
2025-05-24 12:33:39 +0200

    include_graph: fix

--- src/build/include_graph.h
+++ src/build/include_graph.h
@@ -177,8 +177,8 @@ static Include_Node *include_graph_read_file(Include_Graph *graph, String path, 
 
     String read = file;
     for (;;) {
+        if (read.len == 0) break;
         String line = read_line(&read);
-        if (line.len == 0) break;
 
         if (!str_drop_start_matching(&line, S("#include \""))) continue;
         if (!str_drop_end_matching(&line, S("\""))) continue;
