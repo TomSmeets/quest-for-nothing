c9b5352268b823685ccac15cdae729eb0500e18f
Tom Smeets <tom@tsmeets.nl>
2025-06-05 09:22:02 +0200

    monster statemachine

--- src/qfn/monster2.h
+++ src/qfn/monster2.h
@@ -10,21 +10,36 @@
 typedef struct Monster Monster;
 
 typedef enum {
+    // Standing still
     Monster_State_Idle,
+
+    // Moving around
+    Monster_State_Move,
+
+    // Attacking player
     Monster_State_Attack,
+
+    // Fleeing
     Monster_State_Scared,
+
+    // Dead
     Monster_State_Dead,
 } Monster_State;
 
 struct Monster {
     Monster *next;
 
-    f32 state_time;
-    Monster_State state;
-
     v3 pos;
     v2 size;
     f32 health;
+
+    Monster_State state;
+    f32 death_animation;
+    f32 wiggle_amount;
+    f32 wiggle_phase;
+
+    // Direction when moving
+    v3 move_direction;
 
     // Visuals
     Monster_Sprite sprite;
@@ -46,52 +61,79 @@ static void m4_scale_image(m4 *mtx, Image *img) {
     m4_scale(mtx, (v3){img->size.x / 32.0f, img->size.y / 32.0f, 1});
 }
 
-static void monster2_set_state(Monster *mon, Monster_State state) {
-    mon->state = state;
-    mon->state_time = 0;
-}
-
 static void monster2_update(Monster *mon, Engine *eng, v3 player_pos) {
     f32 dt = eng->dt;
     Rand *rng = &eng->rng;
 
-    v3 player_dir = player_pos - mon->pos;
-    f32 player_dist = v3_length(player_dir);
-    if (player_dist < 2) monster2_set_state(mon, Monster_State_Scared);
-    if (player_dist < 1) monster2_set_state(mon, Monster_State_Dead);
+    v3 player_diff = player_pos - mon->pos;
+    f32 player_dist = v3_length(player_diff);
+    v3 player_dir = player_dist > 0 ? player_diff / player_dist : 0;
 
-    bool idle = mon->state == Monster_State_Idle;
-    bool attack = mon->state == Monster_State_Attack;
-    f32 dead = mon->state == Monster_State_Dead ? mon->state_time : 0;
-    f32 scared = mon->state == Monster_State_Scared ? (1 - player_dist / 2) : 0;
-    f32 alive = 1 - dead;
+    switch(mon->state) {
+        case Monster_State_Idle:
+        case Monster_State_Move:
+        case Monster_State_Attack:
+            if(player_dist < 1) mon->state = Monster_State_Scared;
+            break;
+        default:
+            break;
+    }
 
-    mon->state_time = f_min(mon->state_time + dt, 1.0f);
+    // State switching
+    if (mon->state == Monster_State_Idle && rand_choice(rng, 0.4 * dt)) {
+        mon->state = Monster_State_Move;
+        f32 angle = rand_f32(rng, -1, 1);
+        mon->move_direction = (v3){f_cos2pi(angle), 0, f_sin2pi(angle)};
+    if (mon->state == Monster_State_Idle && rand_choice(rng, 0.4 * dt)) {
+        mon->state = Monster_State_Attack;
+    } else if (mon->state == Monster_State_Move && rand_choice(rng, 0.5 * dt)) {
+        mon->state = Monster_State_Idle;
+    } else if (mon->state == Monster_State_Attack && rand_choice(rng, 0.2 * dt)) {
+        mon->state = Monster_State_Idle;
+    } else if (mon->state == Monster_State_Scared && rand_choice(rng, 0.5 * dt)) {
+        mon->state = Monster_State_Idle;
+    }
+
+    f32 look_chance = 0.20f;
+    f32 scared_amount = 0;
+    v3 vel = 0;
+    if (mon->state == Monster_State_Idle) {
+    } else if (mon->state == Monster_State_Move) {
+        vel = mon->move_direction * 0.4;
+    } else if (mon->state == Monster_State_Attack) {
+        if(player_dist > 0) vel = player_dir * 0.8;
+    } else if (mon->state == Monster_State_Scared) {
+        look_chance = 2.0f;
+        scared_amount = 1;
+        if(player_dist > 0) vel = -player_dir * 0.8;
+    } else if (mon->state == Monster_State_Dead) {
+        look_chance = 0.0f;
+        mon->death_animation = f_min(mon->death_animation + dt, 1.0f);
+    }
+
+    mon->wiggle_amount += (v3_length(vel) * .5 - mon->wiggle_amount) * dt;
+    mon->wiggle_phase = f_fract(mon->wiggle_phase + dt*mon->wiggle_amount*8);
+
+    f32 dead_amount = mon->death_animation;
+    f32 wiggle_amount = mon->wiggle_amount;
+    f32 wiggle_phase  = mon->wiggle_phase;
 
     // Look around
-    f32 look_chance = 0.20f;
-    if (scared) look_chance = 2.0f;
-    if (dead) look_chance = 0;
     if (look_chance && rand_choice(rng, dt * look_chance)) {
         monster_sprite_update_eyes(&mon->sprite, rng);
     }
 
-    if (scared && rand_choice(rng, dt * 0.5)) monster2_set_state(mon, Monster_State_Idle);
-    if (attack && rand_choice(rng, dt * 0.1)) monster2_set_state(mon, Monster_State_Idle);
-    if (idle && rand_choice(rng, dt * 0.1)) monster2_set_state(mon, Monster_State_Attack);
-
-    v3 vel = 0;
-    if (attack && player_dist > 0) vel += player_dir / player_dist * 0.20f;
-    if (scared && player_dist > 0) vel -= player_dir / player_dist * 1.00f;
+    // Movement
     mon->pos += vel * dt;
 
+    // Graphics
     m4 mtx_monster = m4_id();
     m4_translate(&mtx_monster, mon->pos);
 
     m4 mtx_rotated = m4_id();
-    // if (!dead) m4_rotate_z(&mtx_rotated, f_sin(mon->state_time*R4) * R1 * 0.3);
-    if (scared) m4_rotate_z(&mtx_rotated, rand_f32(rng, -1, 1) * .08 * scared);
-    if (dead) m4_rotate_x(&mtx_rotated, -R1 * dead);
+    if (wiggle_amount) m4_rotate_z(&mtx_rotated, f_sin(wiggle_phase * R4) * R1 * wiggle_amount * 0.3);
+    if (scared_amount) m4_rotate_z(&mtx_rotated, rand_f32(rng, -1, 1) * .08 * scared_amount);
+    if (dead_amount) m4_rotate_x(&mtx_rotated, -R1 * dead_amount);
     m4_rotate_y(&mtx_rotated, R1 - f_atan2(player_dir.z, player_dir.x));
     m4_apply(&mtx_rotated, mtx_monster);
     m4_translate_y(&mtx_rotated, 2e-3f);
@@ -103,7 +145,7 @@ static void monster2_update(Monster *mon, Engine *eng, v3 player_pos) {
 
     m4 mtx_shadow = m4_id();
     m4_scale_image(&mtx_shadow, mon->sprite.shadow);
-    if (dead) m4_scale(&mtx_shadow, alive);
+    if (dead_amount) m4_scale(&mtx_shadow, 1 - dead_amount);
     m4_rotate_x(&mtx_shadow, -R1);
     m4_translate_y(&mtx_shadow, 1e-3f);
     m4_apply(&mtx_shadow, mtx_monster);
@@ -113,11 +155,11 @@ static void monster2_update(Monster *mon, Engine *eng, v3 player_pos) {
     f32 gun_x = -mon->sprite.hand[0].x / 32.0f * 0.5f * 0.9f;
     m4_scale_image(&mtx_gun, mon->gun);
     m4_translate_x(&mtx_gun, -.1);
-    m4_rotate_y(&mtx_gun, R1 * 0.80f * alive);
+    m4_rotate_y(&mtx_gun, R1 * 0.80f * (1-dead_amount));
     m4_translate_y(&mtx_gun, gun_y);
     m4_translate_x(&mtx_gun, gun_x);
-    if (dead) m4_translate_x(&mtx_gun, -0.4 * dead);
-    if (dead) m4_rotate_z(&mtx_gun, -0.2 * R1 * dead);
+    if (dead_amount) m4_translate_x(&mtx_gun, -0.4 * dead_amount);
+    if (dead_amount) m4_rotate_z(&mtx_gun, -0.2 * R1 * dead_amount);
     m4_apply(&mtx_gun, mtx_rotated);
 
     // monster_sprite_update_eyes(&mon->sprite, rng);
