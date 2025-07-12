d49c4425b5e37bc16a1f02c48641e7e515f14224
Tom Smeets <tom@tsmeets.nl>
2025-07-12 18:18:29 +0200

    gfx2: api

--- src/gfx/gfx2.h
+++ src/gfx/gfx2.h
@@ -1,11 +1,13 @@
 // Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
 // gfx.h - A Graphics, Audio, and Input handling abstraction
 #pragma once
+#include "gfx/image.h"
+#include "gfx/input.h"
+#include "lib/mat.h"
 #include "lib/mem.h"
 #include "lib/vec.h"
-#include "gfx/input.h"
-#include "gfx/image.h"
-#include "lib/mat.h"
+
+#define GFX_AUDIO_RATE 48000
 
 typedef struct Gfx Gfx;
 
@@ -21,8 +23,8 @@ static Input *gfx_begin(Gfx *gfx);
 // Finish Render
 static void gfx_end(Gfx *gfx);
 
-
-static void gfx_draw(Gfx *gfx, bool depth, m4 mtx, Image *img);
+// Draw image during render
+static void gfx_draw(Gfx *gfx, bool depth, Image *img, m4 mtx);
 
 // Audio Callback that runs in a seperate thread (use locking if needed)
 // Fill sample_list with audio samples as they are requested
