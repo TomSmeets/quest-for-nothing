f4c19b779b0dae52fe08d02434a643506d7b25c4
Tom Smeets <tom@tsmeets.nl>
2025-10-01 23:14:28 +0200

    update build

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 if not exist out mkdir out
-clang -O0 -g -I src -o out\build-pre.exe src\build\build.c && out\build-pre.exe %*
+clang -O0 -g -I src -o out\build.exe src\build\build.c && out\build.exe %*
