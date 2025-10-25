b7642bcb5d576c5d722caa3629b16db9cb647554
Tom Smeets <tom@tsmeets.nl>
2025-10-25 22:45:20 +0200

    gfx: fix include

--- src/gfx/gfx.h
+++ src/gfx/gfx.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
 // gfx_impl.h - Automatic GFX Platform implementation selector
 #pragma once
-#include "gfx/gfx.h"
+#include "gfx/gfx_api.h"
 #include "lib/os_main.h"
 #if OS_IS_LINUX || OS_IS_WINDOWS
 #include "gfx/gfx_desktop.h"
