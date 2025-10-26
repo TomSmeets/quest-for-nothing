dc2007f1f0d2e5201320282890b945d9df2f8d49
Tom Smeets <tom@tsmeets.nl>
2025-10-26 11:48:15 +0100

    arg parsing

--- release.sh
+++ release.sh
@@ -1,4 +1,26 @@
+set -euo pipefail
+
+echo $1
+
+rm -vrf out/release
 mkdir -p out/release
+
 ./build build src/qfn/qfn.c out/release/quest_for_nothing.elf  release linux
 ./build build src/qfn/qfn.c out/release/quest_for_nothing.exe  release windows
 ./build build src/qfn/qfn.c out/release/quest_for_nothing.wasm release wasm
+
+# Download SDL3
+if [ ! -f out/SDL3.dll ]; then
+  curl -L -o out/SDL3.zip https://github.com/libsdl-org/SDL/releases/download/release-3.2.16/SDL3-3.2.16-win32-x64.zip
+  ( cd out/ && unzip SDL3.zip SDL3.dll )
+fi
+
+cp out/SDL3.dll       out/release/SDL3.dll
+cp src/lib/*.js       out/release/
+cp src/gfx/*.js       out/release/
+cp src/qfn/index.html out/release/index.html
+
+# Publish release
+# butler push out/release tsmeets/quest-for-nothing:release --userversion $(date +'%F')
+# butler push out/release tsmeets/quest-for-nothing:release-web --userversion $(date +'%F')
+# rclone copy out/release fastmail:tsmeets.fastmail.com/files/tsmeets.nl/qfn/
--- src/qfn/build.c
+++ src/qfn/build.c
@@ -1,103 +1,256 @@
 // Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
 // build.c - Build tool for runing and building applications
-#include "build/build.h"
-#include "build/clang.h"
-#include "build/cli.h"
-#include "build/hot.h"
-#include "build/include_graph.h"
-#include "build/watch.h"
 #include "lib/fmt.h"
+#include "lib/mem.h"
+#include "lib/types.h"
 #include "lib/os_main.h"
 
-struct App {
-    Build *build;
+TYPEDEF_STRUCT(Cli_Option);
 
-    // Hot
-    Hot *hot;
+// ./build <command> [Input0] --option1  [input1] --option2
+//
+typedef enum {
+    Argument_Word,  // [WORD]
+    Argument_Value, // =value
+    Argument_Short, // -f
+    Argument_Long,  // --flag
+} Argument_Type;
 
-    // First time?
-    bool first;
+TYPEDEF_STRUCT(Argument);
+struct Argument {
+    String name;
+    Argument_Type type;
+    bool is_used;
+    bool is_expected;
+    Argument *next;
 };
 
-static bool fs_exists(String path) {
-    File *f = os_open(path, Open_Read);
-    if (!f) return false;
-    os_close(f);
-    return true;
+static Argument *argument_new(Memory *mem, u32 argc, char **argv) {
+    Argument *first = 0;
+    Argument *last = 0;
+    bool no_flags = false;
+    for(u32 i = 1; i < argc; ++i) {
+        if(strz_eq(argv[i], "--")) {
+            no_flags = true;
+            continue;
+            }
+
+        Argument *arg = mem_struct(mem, Argument);
+        arg->name = str_from(argv[i]);
+            arg->type = Argument_Word;
+
+            if(!no_flags) {
+            if (str_starts_with(arg->name, S("--"))) {
+                arg->type = Argument_Long;
+                arg->name = str_drop_start(arg->name, 2);
+            } else if (str_starts_with(arg->name, S("-"))) {
+                arg->type = Argument_Short;
+                arg->name = str_drop_start(arg->name, 1);
+            }
+            }
+
+        LIST_APPEND(first, last, arg);
+    }
+    return first;
 }
 
-static bool build_all(App *app, Cli *cli) {
-    bool build = cli_flag(cli, "release", "Build qfn release");
-    bool upload = cli_flag(cli, "publish", "Upload archive");
-    if (!build && !upload) return false;
-    os_system(S("mkdir -p out/release/"));
-
-    Clang_Options opts = {};
-    opts.includes = app->build->sources;
-    opts.input_path = "src/qfn/qfn.c";
-    opts.release = true;
-
-    opts.platform = Platform_Linux;
-    opts.output_path = "out/release/quest_for_nothing.elf";
-    if (!clang_compile(opts)) os_exit(1);
-
-    opts.platform = Platform_Windows;
-    opts.output_path = "out/release/quest_for_nothing.exe";
-    if (!clang_compile(opts)) os_exit(1);
-
-    opts.platform = Platform_Wasm;
-    opts.output_path = "out/release/quest_for_nothing.wasm";
-    if (!clang_compile(opts)) os_exit(1);
-
-    // Download SDL3
-    if (!fs_exists(S("out/SDL3.dll"))) {
-        os_system(S("curl -L -o out/SDL3.zip https://github.com/libsdl-org/SDL/releases/download/release-3.2.16/SDL3-3.2.16-win32-x64.zip"));
-        os_system(S("cd out/ && unzip SDL3.zip SDL3.dll"));
+static void argument_print(Fmt *fmt, Argument *args) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        bool first = arg == args;
+        u32 cursor = fmt_cursor(fmt);
+        if(!first) fmt_s(fmt, " ");
+        if (arg->type == Argument_Short) fmt_s(fmt, "[s]");
+        if (arg->type == Argument_Long) fmt_s(fmt, "[l]");
+        if (arg->type == Argument_Value) fmt_s(fmt, "[v]");
+        if (arg->type == Argument_Word) fmt_s(fmt, "[w]");
+        if (arg->is_used) fmt_s(fmt, "[u]");
+        fmt_str(fmt, arg->name);
     }
-    os_system(S("cp out/SDL3.dll out/release/SDL3.dll"));
-    os_system(S("cp src/lib/*.js out/release/"));
-    os_system(S("cp src/gfx/*.js out/release/"));
-    os_system(S("cp src/qfn/index.html out/release/index.html"));
+    fmt_s(fmt, "\n");
+}
 
-    if (upload) {
-        os_system(S("butler push out/release tsmeets/quest-for-nothing:release --userversion $(date +'%F')"));
-        os_system(S("butler push out/release tsmeets/quest-for-nothing:release-web --userversion $(date +'%F')"));
-        os_system(S("rclone copy out/release fastmail:tsmeets.fastmail.com/files/tsmeets.nl/qfn/"));
+static void argument_help(Fmt *fmt, Argument *args) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->is_expected) {
+            fmt_s(fmt, "Expecting: ");
+            fmt_str(fmt, arg->name);
+            fmt_s(fmt, "\n");
+        } else if(!arg->is_used) {
+            if (arg->type == Argument_Short) {
+                fmt_s(fmt, "Unknown flag: -");
+                fmt_str(fmt, arg->name);
+                fmt_s(fmt, "\n");
+            }
+
+            if (arg->type == Argument_Long) {
+                fmt_s(fmt, "Unknown flag: --");
+                fmt_str(fmt, arg->name);
+                fmt_s(fmt, "\n");
+            }
+
+            if (arg->type == Argument_Word) {
+                fmt_s(fmt, "Unknown argument: ");
+                fmt_str(fmt, arg->name);
+                fmt_s(fmt, "\n");
+            }
+
+            if (arg->type == Argument_Value) {
+                fmt_s(fmt, "Unknown value: ");
+                fmt_str(fmt, arg->name);
+                fmt_s(fmt, "\n");
+            }
+            if(arg->type == Argument_Word) break;
+        }
     }
-    os_exit(0);
-    return true;
+}
+
+struct Cli_Option {
+    u32 index;
+    String name;
+    String info;
+    Cli_Option *next;
+    Cli_Option *parent;
+};
+
+typedef struct {
+    Memory *mem;
+    Argument *args;
+} Cli;
+
+struct App {
+    Cli *cli;
+};
+
+static Cli *cli_new(void) {
+    Memory *mem = G->mem;
+    Cli *cli = mem_struct(mem, Cli);
+    cli->mem = mem;
+    cli->args =argument_new(mem, G->argc, G->argv);
+    return cli;
+}
+
+
+static void cli_begin(Cli *cli, char *name, char *info) { }
+static void cli_end(Cli *cli) { }
+
+static bool cli_flag(Cli *cli, char *name, char *info) {
+    cli_begin(cli, name, info);
+    cli_end(cli);
+    return false;
+}
+
+static String cli_next(Cli *cli, char *name, char *info) {
+    cli_begin(cli, name, info);
+    cli_end(cli);
+    return S0;
+}
+
+
+// 0. Lex     ./build run -la --test --hello=4
+// 1. Expand ./build run  -l -a
+//
+
+static void argument_expand_short(Argument *args) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->type != Argument_Short) continue;
+
+        while(arg->name.len > 1) {
+            Argument *new = mem_struct(G->mem, Argument);
+            new->name = str_slice(arg->name, arg->name.len - 1, 1);
+            new->type = Argument_Short;
+            arg->name = str_drop_end(arg->name, 1);
+            new->next = arg->next;
+            arg->next = new;
+        }
+    }
+}
+
+static void argument_expand_long(Argument *args) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->type != Argument_Long) continue;
+
+        u32 ix = str_find(arg->name, '=');
+        if(ix == arg->name.len) continue;
+
+        Argument *new = mem_struct(G->mem, Argument);
+        new->name = str_slice(arg->name, ix + 1, arg->name.len - ix - 1);
+        new->type = Argument_Value;
+
+        arg->name = str_slice(arg->name, 0, ix);
+        new->next = arg->next;
+        arg->next = new;
+    }
+}
+
+static String argument_word(Argument *args, String info) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->is_used) continue;
+        if(arg->type != Argument_Word) continue;
+        arg->is_used = true;
+        return arg->name;
+    }
+
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->next) continue;
+        Argument *new = mem_struct(G->mem, Argument);
+        new->name = info;
+        new->type = Argument_Value;
+        new->is_expected = true;
+        new->is_used     = true;
+        arg->next = new;
+        break;
+    }
+
+    return S0;
+}
+
+static bool argument_match(Argument *args, String match) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->is_used) continue;
+        if(arg->type != Argument_Word) continue;
+        if(!str_eq(arg->name, match)) break;
+        arg->is_used = true;
+        return true;
+    }
+    return false;
+}
+
+static bool argument_flag(Argument *args, String name_short, String name_long) {
+    for(Argument *arg = args; arg; arg = arg->next) {
+        if(arg->is_used) continue;
+        if(!(arg->type == Argument_Short && str_eq(arg->name, name_short))) continue;
+        if(!(arg->type == Argument_Long  && str_eq(arg->name, name_long)))  continue;
+        arg->is_used = true;
+        return true;
+    }
+    return false;
 }
 
 static void os_main(void) {
-    App *app = G->app;
+    Argument *args = argument_new(G->mem, G->argc, G->argv);
+    argument_print(G->fmt, args);
+    argument_expand_short(args);
+    argument_expand_long(args);
+    argument_print(G->fmt, args);
 
-    if (!app) {
-        Build *build = build_new();
-        build_add_source(build, S("src"));
-
-        app = G->app = mem_struct(G->mem, App);
-        app->build = build;
-        app->hot = hot_new(G->mem);
+    if(argument_match(args, S("build"))) {
+        String src = argument_word(args, S("Source"));
+        String dst = argument_word(args, S("Output"));
+        bool release = argument_flag(args, S("r"), S("release"));
+        bool linux = argument_flag(args, S("l"), S("linux"));
+        bool windows = argument_flag(args, S("w"), S("windows"));
+        bool wasm = argument_flag(args, S("j"), S("wasm"));
     }
 
-    Cli cli = cli_new(G->argc, G->argv);
-    do {
-        // Release
-        if (build_all(app, &cli)) break;
+    // for (;;) {
+    //     String word = argument_word(args);
+    //     if (word.len == 0) break;
+    //     fmt_s(G->fmt, "WORD: ");
+    //     fmt_str(G->fmt, word);
+    //     fmt_s(G->fmt, "\n");
+    // }
 
-        // Basics
-        if (build_build(app->build, &cli)) break;
-        if (build_run(app->build, &cli)) break;
-
-        // Extras
-        if (build_format(app->build, &cli)) break;
-        if (build_serve(app->build, &cli)) break;
-        if (build_opt_clangd(app->build, &cli)) break;
-        if (build_include_graph(app->build, &cli)) break;
-
-        // failed
-        cli_show_usage(&cli, G->fmt);
-        os_exit(1);
-    } while (0);
-    build_update(app->build);
+    argument_print(G->fmt, args);
+    argument_help(G->fmt, args);
+    os_exit(0);
 }
