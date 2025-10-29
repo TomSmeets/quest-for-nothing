26d1a2482afd2e105efb46010033eb68ae88215b
Tom Smeets <tom@tsmeets.nl>
2025-10-29 21:42:17 +0100

    math: fix missing include

--- src/lib/math.h
+++ src/lib/math.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
 // math.h - float, integer and vector math expressions
 #pragma once
+#include "lib/os_alloc.h"
 #include "lib/types.h"
 
 #define INF (1.0f / 0.0f)
