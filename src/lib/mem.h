b97922451b3072524e613e1a045ffc5ff2562f36
Tom Smeets <tom@tsmeets.nl>
2025-11-26 19:09:30 +0100

    merge mem and chunk

--- src/lib/mem.h
+++ src/lib/mem.h
@@ -1,8 +1,67 @@
 // Copyright (c) 2025 - Tom Smeets <tom@tsmeets.nl>
 // mem.h - Memory allocator
 #pragma once
-#include "lib/chunk.h"
 #include "lib/types.h"
+
+// chunk_cache is a global variable
+#include "lib/global.h"
+
+// Memory can be requested from the operating system at any time.
+#include "lib/os_alloc.h"
+
+// This memory is never given back to the operating system.
+// We are managing it ourselves.
+
+// Memory is allocaed from the operating system in big fixed-size Chunks.
+// - Every chunk is the same and can be reused anywehre
+// - Every chunk should be big enough to hold the biggest structure.
+// - Every chunk is eitehr 'free' or 'used'.
+
+// Every `Chunk` has a fixed size of exactly 1 MB
+#define CHUNK_SIZE (1 * 1024 * 1024)
+#define CHUNK_HEADER_SIZE (sizeof(void *))
+
+typedef struct Chunk Chunk;
+
+struct Chunk {
+    // Each chunk has a small header that can point to the next chunk, creating a linked-list.
+    // The next pointer is embedded in the chunk memory.
+    Chunk *next;
+
+    // The rest of the chunk is memory, used or free
+    // ...
+};
+
+// Allocate a new chunk
+static Chunk *chunk_alloc(void) {
+    // Check if there are free chunks
+    if (G->chunk_cache) {
+        // Remove the free chunk from the cache
+        Chunk *chunk = G->chunk_cache;
+        G->chunk_cache = chunk->next;
+        chunk->next = 0;
+        G->stat_cache_size -= CHUNK_SIZE;
+
+        // Give it to the caller
+        return chunk;
+    }
+
+    G->stat_alloc_size += CHUNK_SIZE;
+
+    // The cache is empty, ask the os for a new chunk.
+    return os_alloc(CHUNK_SIZE);
+}
+
+// Free all used chunks in this list and add the to the cache
+static void chunk_free(Chunk *first) {
+    Chunk *chunk = first;
+    while (chunk) {
+        Chunk *next = chunk->next;
+        LIST_PUSH(G->chunk_cache, chunk);
+        G->stat_cache_size += CHUNK_SIZE;
+        chunk = next;
+    }
+}
 
 typedef struct Memory Memory;
 
